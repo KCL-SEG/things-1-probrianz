d063fbc277ce3300ce9289f488b71278c303f798
Brian Lee <probrian89@gmail.com>
2023-10-04 23:32:49 +0800

    CharField for descrip

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    description = models.TextField(blank=True, max_length=120)
+    description = models.CharField(blank=True, max_length=120)
     quantity = models.PositiveIntegerField(
         default=0,
         validators=[
