f11f984b438b2dd2f4a9f7c2bb83c04af1223904
Brian Lee <probrian89@gmail.com>
2023-10-04 23:25:02 +0800

    new update on html

--- things/views.py
+++ things/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 # things/views.py
 def home(request):
-    return render(request, 'things/templates/things/home.html')
+    return render(request, 'home.html')
